36e02d3477e3e7e5259bea5280d3f30b24b74941
Alexey Lonin <Ininale@gmail.com>
2022-07-11 15:16:13 +0500

    Седьмая задача

---
+++ 007/Program.cs
@@ -0,0 +1,12 @@
+﻿// 7.	Выяснить является ли число чётным
+Console.Write("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+        if ((x % 2) == 0)
+        {
+            Console.WriteLine("чётное");
+        }
+        else
+        {
+            Console.WriteLine("нечётное");
+        }
+return;
