226b49e9c2e5cb20666378440a67207369e9c84a
Alexey Lonin <Ininale@gmail.com>
2022-07-11 15:33:12 +0500

    Пятая задача

---
+++ 005/Program.cs
@@ -0,0 +1,15 @@
+﻿// 5.	Найти максимальное из трех чисел
+int[] Numbers = { 54, 69, 8 };
+
+for (int i = 0; i < 3; i++)
+{
+    int max = Numbers[0];
+    
+    if (Numbers[i] > max)
+    {
+        max = Numbers[i];
+        System.Console.WriteLine();
+        System.Console.WriteLine($"Максимальное число {max}");
+    }
+}
+System.Console.WriteLine();
