712ac8f154914c4b331122a7bd105f9e000dd684
Alexey Lonin <Ininale@gmail.com>
2022-07-11 15:25:02 +0500

    Восьмая задача

---
+++ 008/Program.cs
@@ -0,0 +1,13 @@
+﻿// 8.	Показать числа от -N до N
+System.Console.Write("Введите число:   ");
+string writeNumber = Console.ReadLine();
+int endNumber = Convert.ToInt32(writeNumber);
+int Number = endNumber * (-1);
+System.Console.Write($"{Number} ");
+
+while (Number < endNumber)
+{
+    Number++;
+    System.Console.Write($"{Number} ");
+}
+System.Console.WriteLine(); System.Console.WriteLine();
