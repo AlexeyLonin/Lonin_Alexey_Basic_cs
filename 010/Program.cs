41dc59d0181c6064079fa9bd3b55fc63e1dff6d2
Alexey Lonin <Ininale@gmail.com>
2022-07-11 16:08:58 +0500

    Десятая задача

--- 010/Program.cs
+++ 010/Program.cs
@@ -1,6 +1,4 @@
 ﻿// 10.	Показать последнюю цифру трёхзначного числа
-System.Console.WriteLine();
-
 int number = new Random().Next(100, 1000);
 
 System.Console.WriteLine($"Последней цифрой числа {number} является цифра {number%10}");
