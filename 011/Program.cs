6ee3a26e3715d199df81292f4490789aea714b14
Alexey Lonin <Ininale@gmail.com>
2022-07-11 16:09:20 +0500

    Одиннадцатая задача

--- 011/Program.cs
+++ 011/Program.cs
@@ -1,5 +1,4 @@
 ﻿// 11.	Показать вторую цифру трёхзначного числа
-Console.WriteLine();
 int number = new Random().Next(100, 1000);
 
 Console.WriteLine($"Второй цифрой числа {number} является {number / 10 % 10} ");
