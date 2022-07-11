0a92c3fbc978fbbc188374d54eafe434be705ff1
Alexey Lonin <Ininale@gmail.com>
2022-07-11 14:54:58 +0500

    Шестая задача

---
+++ 006/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+ double f(double x)
+{
+    double k;
+    if (Math.Sin(x) < 0)
+        k = x*x;
+    else
+        k = Math.Abs(x);
+    
+    if (k < x)
+        return k * x;
+    else
+        return k + x;
+}
