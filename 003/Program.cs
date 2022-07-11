a8c98db5d976f1f9432208fdbaad5e4b0716e719
Alexey Lonin <Ininale@gmail.com>
2022-07-11 11:42:43 +0500

    Третья задача

---
+++ 003/Program.cs
@@ -0,0 +1,23 @@
+﻿// 3. Даны два числа. Показать большее и меньшее число
+double a,b;
+string? s=Console.ReadLine();
+a=Convert.ToDouble (s);
+s=Console.ReadLine();
+b=Convert.ToDouble(s);
+
+
+if (a == b)
+{
+Console.WriteLine("Равны");
+}
+else 
+if(a>b)
+{
+Console.WriteLine(a);
+Console.WriteLine(b);
+}
+else
+{
+Console.WriteLine(b);
+Console.WriteLine(a);
+}
