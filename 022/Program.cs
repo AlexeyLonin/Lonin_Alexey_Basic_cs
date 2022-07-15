71ee387a139c178e4c53709a4aea22972f77edbb
Alexey Lonin <Ininale@gmail.com>
2022-07-15 13:42:22 +0500

    двадцать вторая задача

---
+++ 022/Program.cs
@@ -0,0 +1,20 @@
+﻿// 22. Программа проверяет пятизначное число на палиндром.
+System.Console.Write("Введите целое число:   ");
+string number = Console.ReadLine();
+int length = number.Length;
+string txtToCheck = number.Replace(" ","");  /// для проверки текста на палиндром
+
+bool IsPalindrome()
+{
+    for (int i = 0; i < length / 2; i++)
+        while (number[i] == number[length - 1])
+        {
+            return true;
+        }
+    return false;
+}
+
+System.Console.WriteLine("Ответ:");
+if (IsPalindrome()) System.Console.WriteLine($"1. Введный текст/число - {number} - является палиндромом");
+else System.Console.WriteLine($"1. Число {number} не является палиндромом");
+System.Console.WriteLine();
