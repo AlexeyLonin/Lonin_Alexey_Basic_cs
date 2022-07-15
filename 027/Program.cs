9fcc8bdd8340ef48d1e2432252b533fea6284843
Alexey Lonin <Ininale@gmail.com>
2022-07-15 14:08:24 +0500

    двадцать седьмая задача

---
+++ 027/Program.cs
@@ -0,0 +1,15 @@
+﻿// 27. Возведите число А в натуральную степень B, используя цикл
+
+int number = 2;
+int exponent = 10;
+int count = 1;
+int result = number;
+
+while (count < exponent)
+{
+    result = result * number;
+    count++;
+}
+
+System.Console.WriteLine($"{number} ^ {exponent} = {result}");
+System.Console.WriteLine();
