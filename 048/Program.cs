002609a8e272a697871338c1a53e75b4ad694af4
Alexey Lonin <Ininale@gmail.com>
2022-08-09 12:48:23 +0500

    Сорок восьмая задача

---
+++ 048/Program.cs
@@ -0,0 +1,15 @@
+﻿// 048. Написать программу копирования массива
+public class Example
+{
+    public static void Main()
+    {
+        int[] arr = { 1, 4, 8, 9, 3 };
+ 
+        int[] copy = new int[arr.Length];
+        for (int i = 0; i < arr.Length; i++) {
+            copy[i] = arr[i];
+        }
+ 
+        Console.WriteLine(String.Join(", ", copy));        // 4, 7, 2
+    }
+}
