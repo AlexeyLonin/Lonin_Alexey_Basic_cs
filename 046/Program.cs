10e63b8f5e914adb737c0ef57428d840dfda12a6
Alexey Lonin <Ininale@gmail.com>
2022-08-09 12:48:02 +0500

    Сорок шестая задача

---
+++ 046/Program.cs
@@ -0,0 +1,17 @@
+﻿// 046. С клавиатуры вводится число N. 
+// Показать первые N чисел Фибоначчи. Принять первые числа равными 0 и 1
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int perv = 1;
+Console.Write("{0} ", perv);
+int vtor = 1;
+Console.Write("{0} ", vtor);
+int sum = 0;
+
+while (number >= sum)
+    {     sum = perv + vtor;            
+      Console.Write("{0} ", sum);                
+     perv = vtor;
+     vtor = sum;                
+                
+    }
