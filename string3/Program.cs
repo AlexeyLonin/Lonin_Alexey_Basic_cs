736f7fe88485bf1c6c7e141d4ee46947f42e52c7
Alexey Lonin <Ininale@gmail.com>
2022-08-20 18:25:12 +0500

    Строки. 3 задача

---
+++ string3/Program.cs
@@ -0,0 +1,9 @@
+﻿// 3. С клавиатуры вводится строка, разделенная точкой.
+// Подсчитать количество символов до точки
+{
+    Console.Write("Введите текст: ");
+    string text = Console.ReadLine();
+    int before = text.IndexOf('.');
+ 
+    Console.WriteLine($"До точки {before} символов");
+}
